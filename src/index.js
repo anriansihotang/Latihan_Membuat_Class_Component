362041ce4b9876ddbd0fc87a15bc16fc5198e59f
Anrian Sihotang <54989405+anriansihotang@users.noreply.github.com>
2022-06-09 21:12:18 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { createRoot } from 'react-dom/client';
+
+class MyComponent extends Component {
+  constructor(props) {
+    super(props);
+    console.log('Component created!');
+  }
+  render() {
+    return <p>Hello, {this.props.name}</p>;
+  }
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <div>
+    <MyComponent name="Dicoding" />
+    <MyComponent name="John" />
+    <MyComponent name="Doe" />
+  </div>
+);
